52fad1cd845b57798e4465f243f78981087aee4e
aabclara <claram.miguelc03@gmail.com>
2025-06-09 15:53:42 -0300

    Polinomio de Lagrange

---
+++ MariaClaraM2.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def polinomio_lagrange(coordenadas_x, coordenadas_y, x):
+    array_coordenadas_x = np.asarray(coordenadas_x, dtype=float)
+    array_coordenadas_y = np.asarray(coordenadas_y, dtype=float)
+
+    num_pontos = len(array_coordenadas_x)
+
+    if num_pontos != len(array_coordenadas_y):
+        raise ValueError("O número de coordenadas x e y deve ser o mesmo!")
+
+    if len(np.unique(array_coordenadas_x)) != num_pontos:
+        raise ValueError("Os valores de x das coordenadas devem ser distintos para a interpolação de Lagrange!")
+
+    valor_polinomio_lagrange = 0.0  
+
+    for i in range(num_pontos):
+        termo_produtorio = 1.0
+
+        for j in range(num_pontos):
+            if i != j:  
+                termo_atual = (x - array_coordenadas_x[j]) / \
+                              (array_coordenadas_x[i] - array_coordenadas_x[j])
+                termo_produtorio *= termo_atual
+        
+        valor_polinomio_lagrange += array_coordenadas_y[i] * termo_produtorio
+
+    return valor_polinomio_lagrange
+
+##################################################################################
